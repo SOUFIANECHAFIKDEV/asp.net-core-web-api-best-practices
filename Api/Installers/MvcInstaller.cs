f7ceafdc7550f01543e7d2cecc56c1de886bacfd
MEN\SQLI.DotNetUser8 <schafik@sqli.com>
2021-09-27 14:05:29 +0100

    Setting up JWT support (Authentication)

--- Api/Installers/MvcInstaller.cs
+++ Api/Installers/MvcInstaller.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api.Options;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Installers
 {
@@ -9,6 +15,76 @@ namespace Api.Installers
         public void InstallerServices(IServiceCollection services, IConfiguration Configuration)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            /*
+                Setting up JWT support (Authentication)
+            */
+
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind(key: nameof(jwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddAuthentication(configureOptions: x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+             {
+                 x.SaveToken = true;
+                 x.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(key: Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     RequireExpirationTime = false,
+                     ValidateLifetime = true
+                 };
+             });
+
+            /*
+                [Swagger] Registration
+            */
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Best Implementation API", Version = "v1" });
+
+                #region Setting up JWT support (Authentication)
+
+                x.AddSecurityDefinition(name: "Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
+                #endregion
+            });
         }
     }
 }
---
+++ Api/Options/JwtSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Options
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }
+    }
+}
