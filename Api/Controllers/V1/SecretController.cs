a460199384af2499881bea3acb852c8230eeba51
MEN\SQLI.DotNetUser8 <schafik@sqli.com>
2021-10-12 23:45:01 +0100

    Setting up ApiKey-based Authentication

---
+++ Api/Controllers/V1/SecretController.cs
@@ -0,0 +1,15 @@
+﻿using Api.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpPost("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have on secrets");
+        }
+    }
+}
---
+++ Api/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncResultFilter
+    {
+        private const string ApiKeyHeaderName = "Apikey";
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apikey = configuration.GetValue<string>("Apikey");
+
+            if (!apikey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
